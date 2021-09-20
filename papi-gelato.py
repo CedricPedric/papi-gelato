5e0095565fc89163585df37a85d2aaacdd8f0984
CedricPedric <99059069@mydavinci.nl>
2021-09-20 19:31:33 +0200

    Commit met smaken functie

--- papi-gelato.py
+++ papi-gelato.py
@@ -18,16 +18,49 @@ def YesOrNoVraag(vraag, optionsTrue = ['yes','ja', 'A'], optionsFalse = ['no', '
         else:
             print('Dat begrijp ik niet')
 
+def smaak(smaakvraag):
+    inputSmaak = input(smaakvraag)
+    if inputSmaak == 'A':
+        return 'Aardbei'
+    elif inputSmaak == 'C':
+        return 'Chocolade'
+    elif inputSmaak == 'M':
+        return 'Munt'
+    elif inputSmaak == 'V':
+        return 'Vanille'
+    else:
+        print('Sorry dat snap ik niet...')
+        return smaak(smaakvraag)
+
 while True:
     aantal = hoeveelijs()
+    aantalbollen = aantal
+    allesmaken = ""
+
+    for x in range(aantalbollen):
+        smaken = smaak('Welke smaak wilt u voor bolletje nummer '+ str(aantalbollen) + ' A) Aardbei, C) Chocolade, M) Munt of V) Vanille?: ')
+        aantalbollen = aantalbollen - 1
+        allesmaken = allesmaken + " " + smaken
+
     if aantal >= 1 and aantal <= 3:
         if YesOrNoVraag('Wilt u deze ' + str(aantal) +'  bolletje(s) in A) een hoorntje of B) een bakje?'):
-            print('Hier us u hoorntje met ' + str(aantal) + ' zoveel bollen')
+            print('Hier us u hoorntje met ' + str(aantal) + ' bolletje(s) ')
+            print('de smaken die u heeft gekozen zijn smaken ' + str(allesmaken))
         else:
-            print('Hier us u bakje met ' + str(aantal) + ' zoveel bollen')
+            print('Hier us u bakje met ' + str(aantal) + ' bolletje(s) ')
+            print('de smaken die u heeft gekozen zijn smaken ' + str(allesmaken))
+
         if YesOrNoVraag('Wil u meer ijs bestellen? yes or no: '):
             print("")
         else:
             print('Bedankt en totziens!')
+            break
 
-
+    elif aantal >=4:
+        print('Hier us u bakje met ' + str(aantal) + ' bolletje(s) ')
+        print('de smaken die u heeft gekozen zijn smaken ' + str(allesmaken))
+        if YesOrNoVraag('Wil u meer ijs bestellen? yes or no: '):
+            print("")
+        else:
+            print('Bedankt en totziens!')
+            break
